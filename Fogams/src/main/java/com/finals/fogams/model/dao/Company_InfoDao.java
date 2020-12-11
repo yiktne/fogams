c4baa7fc6a96c70ade32e14dbda2e3c89e2c86a6
Raon <feeljoon1103@gmail.com>
2020-12-11 21:47:34 +0900

    imgupload
    upload

---
+++ Fogams/src/main/java/com/finals/fogams/model/dao/Company_InfoDao.java
@@ -0,0 +1,15 @@
+package com.finals.fogams.model.dao;
+
+import java.util.List;
+
+import com.finals.fogams.model.dto.Company_InfoDto;
+
+public interface Company_InfoDao {
+	
+	String NAMESPACE = "com.companyinfo";
+	
+	public List<Company_InfoDto> selectList(int company_no);
+	
+	public int insert(Company_InfoDto dto);
+
+}
