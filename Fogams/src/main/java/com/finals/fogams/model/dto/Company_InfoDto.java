0aaca302785901b8ab4a97c7b7f5de1b2239b04c
NightFox <akanakrk@gmail.com>
2020-12-12 15:59:20 +0900

    Revert "Merge branch 'Raon'"
    This reverts commit 9538198860f280a88983d56187b031e6805f4a5b, reversing
    changes made to 825cf4b663fe0a540e3cfb0ed1c6da1648f99527.

--- Fogams/src/main/java/com/finals/fogams/model/dto/Company_InfoDto.java
+++ Fogams/src/main/java/com/finals/fogams/model/dto/Company_InfoDto.java
@@ -1,7 +1,5 @@
 package com.finals.fogams.model.dto;
 
-import org.springframework.web.multipart.MultipartFile;
-
 public class Company_InfoDto {
 
 	private int company_no;
@@ -10,20 +8,18 @@ public class Company_InfoDto {
 	private String company_number;
 	private String company_addr1;
 	private String company_addr2;
-	private String company_img;
 
 	public Company_InfoDto() {
 	}
 
 	public Company_InfoDto(int company_no, String company_content, String company_tel, String company_number,
-			String company_addr1, String company_addr2, String company_img) {
+			String company_addr1, String company_addr2) {
 		this.company_no = company_no;
 		this.company_content = company_content;
 		this.company_tel = company_tel;
 		this.company_number = company_number;
 		this.company_addr1 = company_addr1;
 		this.company_addr2 = company_addr2;
-		this.company_img = company_img;
 	}
 
 	public int getCompany_no() {
@@ -72,14 +68,6 @@ public class Company_InfoDto {
 
 	public void setCompany_addr2(String company_addr2) {
 		this.company_addr2 = company_addr2;
-	}
-
-	public String getCompany_img() {
-		return company_img;
-	}
-
-	public void setCompany_img(String company_img) {
-		this.company_img = company_img;
 	}
 
 }
